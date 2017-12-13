27cf208b5a89a615fd4260ea70e688d94da44069
Clell J. (CJ) Solomon <csolomon@lanl.gov>
2017-12-13 08:41:42 -0700

    Changed FPreProcessor function interface
    Now use keywords (maybe) followed by values.  Keywords are
    
      - DEFINE: sets fpp define flags, e.g., -D<VALUE>
      - UNDEFINE: sets fpp undefine flags, e.g., -U<VALUE>
      - MACROS: sets fpp "-macros" option
      - NOMACROS: sets fpp "-nomacros" option
      - BLANKS: sets fpp "-blanks" option
      - NOBLANKS: sets fpp "-noblanks" option
    
    modified:   FPreProcessor.cmake

--- FPreProcessor.cmake
+++ FPreProcessor.cmake
@@ -3,11 +3,42 @@ find_package(Perl REQUIRED)
 set(FPP_PATH ${CMAKE_CURRENT_LIST_DIR}/utils/fpp.pl CACHE string "path to F preprocessor")
 
 function(FPreProcess infile outfile)
-string(REPLACE ";" " -D" FPP_ARGS "-D${ARGN}")
-add_custom_command(
-  OUTPUT ${outfile}
-  COMMAND ${PERL_EXECUTABLE} ${FPP_PATH} ${FPP_ARGS} -o ${outfile} -- ${infile}
-  DEPENDS ${infile}
-  WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
-)
+  foreach(arg ${ARGN})
+    if(arg STREQUAL "DEFINE")
+      set(keyword "DEFINE")
+    elseif(arg STREQUAL "UNDEFINE")
+      set(keyword "UNDEFINE")
+    elseif(arg STREQUAL "MACROS")
+      set(keyword "MACROS")
+      list(APPEND FPP_ARGS "-macros")
+    elseif(arg STREQUAL "NOMACROS")
+      set(keyword "NOMACROS")
+      list(APPEND FPP_ARGS "-nomacros")
+    elseif(arg STREQUAL "BLANKS")
+      set(keyword "BLANKS")
+      list(APPEND FPP_ARGS "-blanks")
+    elseif(arg STREQUAL "NOBLANKS")
+      set(keyword "NOBLANKS")
+      list(APPEND FPP_ARGS "-noblanks")
+    elseif(arg STREQUAL "SUFFIX")
+      message(STATUS "INVOCATION: FPreProcess(${ARGV})")
+      message(FATAL_ERROR "FPreProcessor -suffix option not supported")
+    else()
+      if(keyword STREQUAL "DEFINE")
+        list(APPEND FPP_ARGS "-D${arg}")
+      elseif(keyword STREQUAL "UNDEFINE")
+        list(APPEND FPP_ARGS "-U${arg}")
+      else()
+        message(STATUS "INVOCATION: FPreProcess(${ARGV})")
+        message(FATAL_ERROR "unknown FPreProcessor keyword ${arg}")
+      endif()
+    endif()
+  endforeach()
+  
+  add_custom_command(
+    OUTPUT ${outfile}
+    COMMAND ${PERL_EXECUTABLE} ${FPP_PATH} ${FPP_ARGS} -o ${outfile} -- ${infile}
+    DEPENDS ${infile}
+    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+  )
 endfunction(FPreProcess)
