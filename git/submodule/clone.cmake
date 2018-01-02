700abd2e0721135b0b2172f7355e0f70e3ed6ca1
Austin McCartney <amccartney@lanl.gov>
2018-01-02 15:12:47 -0700

    rmoved directories

--- git/submodule/clone.cmake
+++
@@ -1,5 +0,0 @@
-function(git_submodule_clone name)
-  execute_process(
-    COMMAND "${GIT_EXECUTABLE} submodule update --init -- ${${name}.path}"
-    WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
-endfunction()
