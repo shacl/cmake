8ba1f668f39b37591bfe16418752509c7c4f2471
Austin McCartney <amccartney@lanl.gov>
2018-01-05 15:34:56 -0700

    introduced additional directory

--- git/submodule/packages.cmake
+++ git/submodule/packages.cmake
@@ -21,11 +21,11 @@ if(NOT git.submodule.packages.cmake)
     "if git.submodule.update is ON, <submodule>.submodule.branch will default to this value.")
 
   find_package(git REQUIRED)
-  include(git/submodule/collect_state)
-  include(git/submodule/init)
-  include(git/submodule/update)
-  include(git/submodule/list)
-  include(git/submodule/find_package)
+  include(git/submodule/packages/collect_state)
+  include(git/submodule/packages/init)
+  include(git/submodule/packages/update)
+  include(git/submodule/packages/list)
+  include(git/submodule/packages/find_package)
 
   set_property(
     GLOBAL PROPERTY git.packages.list.cmake
