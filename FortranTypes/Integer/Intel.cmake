1eab8bef4cc845d2c02ca81387a4c1a581d695c0
Austin Paul McCartney <amccartney@lanl.gov>
2018-01-31 09:10:11 -0700

    updated Fortran Types flags and supressed sourceless target warning

--- FortranTypes/Integer/Intel.cmake
+++ FortranTypes/Integer/Intel.cmake
@@ -1,11 +1,11 @@
 string(CONCAT generator
   "$<$<STREQUAL:${CMAKE_Fortran_COMPILER_ID},Intel>"
     ":$<$<NOT:$<PLATFORM_ID:Windows>>"
-       ":$<$<BOOL:FortranTypes.integer.32>:-integer-size 32>"
-        "$<$<BOOL:FortranTypes.integer.64>:-integer-size 64>>"
+       ":$<$<BOOL:${FortranTypes.integer.32}>:-integer-size;32>;"
+        "$<$<BOOL:${FortranTypes.integer.64}>:-integer-size;64>>;"
      "$<$<PLATFORM_ID:Windows>"
-       ":$<$<BOOL:FortranTypes.integer.32>:/integer-size:32>"
-        "$<$<BOOL:FortranTypes.integer.64>:/integer-size:64>>>")
+       ":$<$<BOOL:${FortranTypes.integer.32}>:/integer-size:32>;"
+        "$<$<BOOL:${FortranTypes.integer.64}>:/integer-size:64>>>")
 
 target_compile_options(Fortran_Integer_Fortran INTERFACE ${generator})
 
--- FortranTypes/Real/GNU.cmake
+++ FortranTypes/Real/GNU.cmake
@@ -1,6 +1,6 @@
 string(CONCAT generator
   "$<$<STREQUAL:${CMAKE_Fortran_COMPILER_ID},GNU>"
-    ":$<$<BOOL:FortranTypes.real.64>:-fdefault-real-8;-fdefault-double-8>>")
+    ":$<$<BOOL:${FortranTypes.real.64}>:-fdefault-real-8;-fdefault-double-8>>")
 
 target_compile_options(Fortran_Real_Fortran INTERFACE ${generator})
 
