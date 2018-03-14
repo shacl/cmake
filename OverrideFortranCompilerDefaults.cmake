43d7161f1860ef97bdfea4a06e5c1db74d4c5eee
Clell J. (CJ) Solomon <csolomon@lanl.gov>
2018-03-14 07:45:22 -0600

    Fixed illegal target names and added meta target
    The interface library target names have been updated to:
      - no_backslash_escape
      - standard_semantics
      - fortran_2003
      - fortran_3008
    Each has an alias with the 'ofcd::' prefix.
    
    The meta target ofcd with the alias ofcd::ofcd has also been provided
    that interfaces no_backslash_escape and standard_semantics.

--- OverrideFortranCompilerDefaults.cmake
+++ OverrideFortranCompilerDefaults.cmake
@@ -4,3 +4,11 @@ include_guard(GLOBAL)
 include(OverrideFortranCompilerDefaults/NoBackslashEscape.cmake)
 include(OverrideFortranCompilerDefaults/StandardAdherence.cmake)
 
+add_library(ofcd INTERFACE)
+add_library(ofcd::ofcd ALIAS ofcd)
+
+target_link_libraries(ofcd INTERFACE 
+  no_backslash_escape
+  standard_semantics
+)
+
--- OverrideFortranCompilerDefaults/NoBackslashEscape.cmake
+++ OverrideFortranCompilerDefaults/NoBackslashEscape.cmake
@@ -1,8 +1,10 @@
 include(Backports/IncludeGuard)
 include_guard(GLOBAL)
 
-add_library(ofcd::no_backslash_escape INTERFACE)
-target_compile_options(ofcd::no_backslash_escape INTERFACE
+add_library(no_backslash_escape INTERFACE)
+add_library(ofcd::no_backslash_escape ALIAS no_backslash_escape)
+
+target_compile_options(no_backslash_escape INTERFACE
   # treat backslashes as normal (i.e., not escape characters)
   $<$<STREQUAL:Flang,${CMAKE_Fortran_COMPILER_ID}>:-fno-backslash>
   $<$<STREQUAL:PGI,${CMAKE_Fortran_COMPILER_ID}>:-Mbackslash>
