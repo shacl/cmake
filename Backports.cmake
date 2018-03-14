702ecad50661b4fb44b2640bdad2a8892429c466
David A. Dixon <ddixon@lanl.gov>
2018-03-14 15:11:52 -0600

    Backports.cmake edited online with Bitbucket

--- Backports.cmake
+++ Backports.cmake
@@ -1,6 +1,4 @@
-if(CMAKE_SIZEOF_VOID_P EQUAL 8)
-  set_property( GLOBAL PROPERTY FIND_LIBRARY_USE_LIB64_PATHS ON )
-endif()
+set_property( GLOBAL PROPERTY FIND_LIBRARY_USE_LIB64_PATHS ON )
 
 include(${CMAKE_CURRENT_LIST_DIR}/Backports/OpenMP.cmake)
 include(${CMAKE_CURRENT_LIST_DIR}/Backports/MPI.cmake)
