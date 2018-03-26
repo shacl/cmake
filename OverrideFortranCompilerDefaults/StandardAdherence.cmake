5e042360956184a83c1ddf0588115af6a35479cd
Clell J. (CJ) Solomon <csolomon@lanl.gov>
2018-03-26 10:28:36 -0600

    Added testing for standard-semantics
    Also, added Intel assume nostd_mod_proc_name that is set with
    standard-semantics.

--- OverrideFortranCompilerDefaults/StandardAdherence.cmake
+++ OverrideFortranCompilerDefaults/StandardAdherence.cmake
@@ -5,8 +5,8 @@ include_guard(GLOBAL)
 
 string(CONCAT generator
   "$<$<STREQUAL:Intel,${CMAKE_Fortran_COMPILER_ID}>:"
-      "$<$<NOT:$<PLATFORM_ID:Windows>>:-standard-semantics>"
-          "$<$<PLATFORM_ID:Windows>:/standard-semantics>>")
+      "$<$<NOT:$<PLATFORM_ID:Windows>>:-standard-semantics;-assume;nostd_mod_proc_name>"
+          "$<$<PLATFORM_ID:Windows>:/standard-semantics;/assume:nostd_mod_proc_name>>")
 
 add_library(standard_semantics INTERFACE)
 add_library(ofcd::standard_semantics ALIAS standard_semantics)
@@ -14,34 +14,3 @@ target_compile_options(standard_semantics INTERFACE
   ${generator}
 )
 
-# Standard-enforcement checks
-
-# Fortran 2003
-
-string(CONCAT generator
-  "$<$<STREQUAL:Intel,${CMAKE_Fortran_COMPILER_ID}>:"
-      "$<$<NOT:$<PLATFORM_ID:Windows>>:-stand;f03>"
-          "$<$<PLATFORM_ID:Windows>:/stand:f03>>"
-          
-  "$<$<STREQUAL:GNU,${CMAKE_Fortran_COMPILER_ID}>:-std=f2003")
-
-add_library(fortran_2003 INTERFACE)
-add_library(ofcd::fortran_2003 ALIAS fortran_2003)
-target_compile_options(fortran_2003 INTERFACE
-  ${generator}
-)
-
-# Fortran 2008
-
-string(CONCAT generator
-  "$<$<STREQUAL:Intel,${CMAKE_Fortran_COMPILER_ID}>:"
-      "$<$<NOT:$<PLATFORM_ID:Windows>>:-stand;f08>"
-          "$<$<PLATFORM_ID:Windows>:/stand:f08>>"
-          
-  "$<$<STREQUAL:GNU,${CMAKE_Fortran_COMPILER_ID}>:-std=f2008")
-
-add_library(fortran_2008 INTERFACE)
-add_library(ofcd::fortran_2008 ALIAS fortran_2008)
-target_compile_options(fortran_2008 INTERFACE
-  ${generator}
-)
