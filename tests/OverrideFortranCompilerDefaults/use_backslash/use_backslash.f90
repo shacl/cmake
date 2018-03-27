28cedbbb5c1e61f8bd32da6807244924da963bad
Clell J. (CJ) Solomon <csolomon@lanl.gov>
2018-03-27 08:04:14 -0600

    Removed redirect from use_backslash tests
    The custom command to run the backslash tests uses a redirect which may
    not be portable.  The generation of the file was moved into the Fortran
    source code.

--- tests/OverrideFortranCompilerDefaults/use_backslash/use_backslash.f90
+++ tests/OverrideFortranCompilerDefaults/use_backslash/use_backslash.f90
@@ -2,5 +2,7 @@ program main
   use iso_fortran_env
   implicit none
 
-  write(output_unit,'(A)') "hello\nworld"
+  open(unit=42, file="terminal_output", status="unknown")
+  write(42,'(A)') "hello\nworld"
+  close(42)
 end program main
