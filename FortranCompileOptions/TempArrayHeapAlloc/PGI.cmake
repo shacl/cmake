73d24c563d8081b738aaba485338b7c6b3eb9e0a
Clell J. (CJ) Solomon <csolomon@lanl.gov>
2018-03-27 12:47:28 -0600

    Removed unused TempArrayHeapAlloc files
    These files were not actually used in the implementation and should have
    been removed earlier.

--- FortranCompileOptions/TempArrayHeapAlloc/PGI.cmake
+++
@@ -1,6 +0,0 @@
-string(CONCAT generator
-  "$<$<BOOL:$<TARGET_PEROPERTY:Fortran_TempArrayHeapAlloc,Fortran_TempArrayHeapAlloc_SIZE>:"
-    "$<STREQUAL:PGI,${CMAKE_Fortran_COMPILER_ID}>:-Mnostack_arrays>"
-)
-
-target_compile_options(Fortran_TempArrayHeapAlloc INTERFACE ${generator})
