538945f47e27121e7a8977163b8d9dd9c213a85e
Clell J. (CJ) Solomon <csolomon@lanl.gov>
2018-03-27 07:34:47 -0600

    Added test to TempArrayHeapAlloc.cmake to prevent generating same target
    Previously, if a Fortran_TempArrayHeapAlloc_<SIZE> target was generated
    and the user tried to generate the same target again, CMake would fail.
    These changes now test for the existence of the target before trying to
    create it.

--- FortranCompileOptions/TempArrayHeapAlloc.cmake
+++ FortranCompileOptions/TempArrayHeapAlloc.cmake
@@ -6,19 +6,21 @@ function(MakeTempArrayHeapAllocTarget size)
   set(size_kbytes ${size})
   math(EXPR size_bytes "${size} * 1000")
 
-  add_library(Fortran_TempArrayHeapAlloc_${size} INTERFACE)
-  add_library(Fortran::TempArrayHeapAlloc_${size} ALIAS Fortran_TempArrayHeapAlloc_${size})
+  if( NOT TARGET Fortran_TempArrayHeapAlloc_${size} )
+    add_library(Fortran_TempArrayHeapAlloc_${size} INTERFACE)
+    add_library(Fortran::TempArrayHeapAlloc_${size} ALIAS Fortran_TempArrayHeapAlloc_${size})
 
-  target_compile_options(Fortran_TempArrayHeapAlloc_${size} INTERFACE
-    "$<$<STREQUAL:GNU,${CMAKE_Fortran_COMPILER_ID}>:-fmax-stack-var-size=${size_bytes}>"
+    target_compile_options(Fortran_TempArrayHeapAlloc_${size} INTERFACE
+      "$<$<STREQUAL:GNU,${CMAKE_Fortran_COMPILER_ID}>:-fmax-stack-var-size=${size_bytes}>"
 
-    "$<$<STREQUAL:Flang,${CMAKE_Fortran_COMPILER_ID}>:-fmax-stack-var-size=${size_bytes}>"
+      "$<$<STREQUAL:Flang,${CMAKE_Fortran_COMPILER_ID}>:-fmax-stack-var-size=${size_bytes}>"
 
-    "$<$<STREQUAL:Intel,${CMAKE_Fortran_COMPILER_ID}>:"
-      "$<$<NOT:$<PLATFORM_ID:Windows>>:-heap-arrays;${size_kbytes}>"
-      "$<$<PLATFORM_ID:Windows>:/heap-arrays:${size_kbytes}>"
-    ">"
+      "$<$<STREQUAL:Intel,${CMAKE_Fortran_COMPILER_ID}>:"
+        "$<$<NOT:$<PLATFORM_ID:Windows>>:-heap-arrays;${size_kbytes}>"
+        "$<$<PLATFORM_ID:Windows>:/heap-arrays:${size_kbytes}>"
+      ">"
 
-    "$<$<STREQUAL:PGI,${CMAKE_Fortran_COMPILER_ID}>:-Mnostack_arrays>"
-  )
+      "$<$<STREQUAL:PGI,${CMAKE_Fortran_COMPILER_ID}>:-Mnostack_arrays>"
+    )
+  endif()
 endfunction()
