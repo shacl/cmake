0dd054966f724ce3ff92125483672dbf536abc79
Austin McCartney <amccartney@lanl.gov>
2018-03-28 12:22:48 -0600

    Alternative impl for TempArrayHeapAlloc
    This implementation assumes the threshold is in kbytes and pads
    appropriate zeros where needed.

--- FortranCompileOptions/TempArrayHeapAlloc.cmake
+++ FortranCompileOptions/TempArrayHeapAlloc.cmake
@@ -1,39 +1,13 @@
 include(Backports/IncludeGuard)
-include(FunctionExtension)
 include_guard(GLOBAL)
 
-backup(set_target_properties)
-function(set_target_properties)
-  list(FIND ARGN "PROPERTIES" pstart)
-  
-  foreach(i RANGE 0 ${pstart})
-    list(GET ARGN ${i} target)
-    list(APPEND forward ${target})
-  endforeach()
+define_property(TARGET PROPERTY Fortran_TempArrayHeapAlloc_THRESHOLD
+  BRIEF_DOCS
+  "A threshold (in kilobytes) above which temporary and automatic arrays should be heap allocated"
+  FULL_DOCS
+  "A threshold (in kilobytes) above which temporary and automatic arrays should be heap allocated"
+)
 
-  math(EXPR pstart "${pstart} + 1")
-  list(LENGTH ARGN plast)
-  math(EXPR plast "${plast} - 1")
-
-  foreach(i RANGE ${pstart} ${plast} 2)
-    math(EXPR iv "${i} + 1")
-    list(GET ARGV ${i} property)
-    list(GET ARGV ${iv} value)
-
-    list(APPEND forward ${property} ${value})
-    if( "${property}" STREQUAL "Fortran_TempArrayHeapAlloc_THRESHOLD_BYTES" )
-      math(EXPR kbytes "${value} / 1000")
-      list(APPEND forward "Fortran_TempArrayHeapAlloc_THRESHOLD_KBYTES" "${kbytes}")
-    endif()
-
-    if( "${property}" STREQUAL "Fortran_TempArrayHeapAlloc_THRESHOLD_KBYTES" )
-      math(EXPR bytes "${value} * 1000")
-      list(APPEND forward "Fortran_TempArrayHeapAlloc_THRESHOLD_BYTES" "${bytes}")
-    endif()
-  endforeach()
-
-  previous_set_target_properties(${forward})
-endfunction()
 
 add_library(Fortran_TempArrayHeapAlloc INTERFACE)
 add_library(FortranCompileOptions::TempArrayHeapAlloc ALIAS Fortran_TempArrayHeapAlloc)
