10a1aca92956a3c59bc9535f7fa23c780a01b32b
Austin McCartney <amccartney@lanl.gov>
2018-03-28 12:30:37 -0600

    documented the property used by TempArrayHeapAlloc

--- FortranCompileOptions/TempArrayHeapAlloc.cmake
+++ FortranCompileOptions/TempArrayHeapAlloc.cmake
@@ -1,6 +1,11 @@
 include(Backports/IncludeGuard)
 include_guard(GLOBAL)
 
+define_property(TARGET PROPERTY Fortran_TempArrayHeapAlloc_THRESHOLD
+  BRIEF_DOCS
+  "A threshold (in kilobytes) above which temporary and automatic arrays should be heap allocated"
+  FULL_DOC "")
+
 add_library(Fortran_TempArrayHeapAlloc INTERFACE)
 add_library(FortranCompileOptions::TempArrayHeapAlloc ALIAS Fortran_TempArrayHeapAlloc)
 
