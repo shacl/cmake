2a0be005aa588064d8aacd6392c9feeebc8a53c9
Austin McCartney <amccartney@lanl.gov>
2018-06-26 16:51:35 -0600

    fixed Generated sources tests

--- GeneratedSources/add_executable.cmake
+++ GeneratedSources/add_executable.cmake
@@ -5,14 +5,14 @@ function(add_executable target)
 
   stripped(${target})
   previous_add_library(${stripped_target_name}.generated_sources.PUBLIC INTERFACE)
-  target_link_libraries(${target} PUBLIC 
+  target_link_libraries(${target} PUBLIC
     $<BUILD_INTERFACE:${stripped_target_name}.generated_sources.PUBLIC>)
 
   previous_add_library(${stripped_target_name}.generated_sources.PRIVATE INTERFACE)
-  target_link_libraries(${target} PRIVATE 
+  target_link_libraries(${target} PRIVATE
     $<BUILD_INTERFACE:${stripped_target_name}.generated_sources.PRIVATE>)
 
   previous_add_library(${stripped_target_name}.generated_sources.INTERFACE INTERFACE)
-  target_link_libraries(${target} INTERFACE 
+  target_link_libraries(${target} INTERFACE
     $<BUILD_INTERFACE:${stripped_target_name}.generated_sources.INTERFACE>)
 endfunction()
