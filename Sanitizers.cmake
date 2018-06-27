74a7efcb09e0141c32c08044c4572831050edda5
Austin McCartney <amccartney@lanl.gov>
2018-06-27 17:19:06 -0600

    sanitizer testing

--- Sanitizers.cmake
+++ Sanitizers.cmake
@@ -2,60 +2,67 @@ include(Backports/IncludeGuard)
 include_guard(GLOBAL)
 
 define_property(TARGET PROPERTY asan
-  BRIEF_DOCS "Enable address sanitizer")
+  BRIEF_DOCS "Enable address sanitizer"
+  FULL_DOCS "AddressSanitizer (aka ASan) is a tool that detects memory error")
 define_property(TARGET PROPERTY msan
-  BRIEF_DOCS "Enable memory sanitizer")
+  BRIEF_DOCS "Enable memory sanitizer"
+  FULL_DOCS "MemorySanitizer (aka MSan) is a tool that detects reads from uninitialized memory")
 define_property(TARGET PROPERTY tsan
-  BRIEF_DOCS "Enable thread sanitizer")
+  BRIEF_DOCS "Enable thread sanitizer"
+  FULL_DOCS "ThreadSanitizer (aka TSan) is a tool that detects data races.")
 define_property(TARGET PROPERTY ubsan
-  BRIEF_DOCS "Enable undefined behavior sanitizer")
+  BRIEF_DOCS "Enable undefined behavior sanitizer"
+  FULL_DOCS "UndefinedBehaviorSanitizer (aka UBSan) is a tool that detects undefined behavior")
 
 option(asan.default
-  "Default address sanitizer behavior (ON/OFF)")
+  "Default address sanitizer behavior (ON/OFF)" OFF)
 mark_as_advanced(asan.default)
+
 option(msan.default
-  "Default memory sanitizer behavior (ON/OFF)")
+  "Default memory sanitizer behavior (ON/OFF)" OFF)
 mark_as_advanced(msan.default)
+
 option(tsan.default
-  "Default thread sanitizer behavior (ON/OFF)")
+  "Default thread sanitizer behavior (ON/OFF)" OFF)
 mark_as_advanced(tsan.default)
+
 option(ubsan.default
-  "Default thread sanitizer behavior (ON/OFF)")
+  "Default thread sanitizer behavior (ON/OFF)" OFF)
 mark_as_advanced(ubsan.default)
 
 add_library(sanitizers INTERFACE)
-add_library(sanitizers_C INTERFACE)
-add_library(sanitizers_CXX INTERFACE)
-add_library(sanitizers_Fortran INTERFACE)
-
 add_library(shacl::sanitizers ALIAS sanitizers)
-add_library(shacl::sanitizers_C ALIAS sanitizers_C)
-add_library(shacl::sanitizers_CXX ALIAS sanitizers_CXX)
-add_library(shacl::sanitizers_Fortran ALIAS sanitizers_Fortran)
 
 set(asan address)
 set(msan memory)
 set(tsan thread)
 set(ubsan undefined)
-
 set(compilation_generator)
 set(linking_generator)
+
 foreach(sanitizer IN ITEMS asan msan tsan ubsan)
   string(CONCAT compilation_generator
     "${compilation_generator}"
-    "$<$<OR:$<BOOL:$<TARGET_PROPERTY:${sanitizer}>>,
-            $<BOOL:${sanitizer}.default>"
-      ">:fsanitize=${${sanitizer}};fno-omit-frame-pointer>")
+    "$<$<OR:$<BOOL:$<TARGET_PROPERTY:${sanitizer}>>,"
+           "$<BOOL:${${sanitizer}.default}>"
+     ">:-fsanitize=${${sanitizer}};-fno-omit-frame-pointer;-fno-sanitize-recover=all>;")
 
   string(CONCAT linking_generator
     "${linking_generator}"
-    "$<$<OR:$<BOOL:$<TARGET_PROPERTY:${sanitizer}>>,
-            $<BOOL:${sanitizer}.default>"
-      ">:fsanitize=${${sanitizer}}>")
+    "$<$<OR:$<BOOL:$<TARGET_PROPERTY:${sanitizer}>>,"
+           "$<BOOL:${${sanitizer}.default}>"
+      ">:-fsanitize=${${sanitizer}}>;")
 endforeach()
 
 target_compile_options(sanitizers INTERFACE ${compilation_generator})
 target_link_libraries(sanitizers INTERFACE ${linking_generator})
+
+add_library(sanitizers_C INTERFACE)
+add_library(sanitizers_CXX INTERFACE)
+add_library(sanitizers_Fortran INTERFACE)
+add_library(shacl::sanitizers_C ALIAS sanitizers_C)
+add_library(shacl::sanitizers_CXX ALIAS sanitizers_CXX)
+add_library(shacl::sanitizers_Fortran ALIAS sanitizers_Fortran)
 
 foreach(language IN ITEMS C CXX Fortran)
   set(linking_generator)
