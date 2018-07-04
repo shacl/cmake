9484927792767c0d4f240084e0fc13b2fc5eec8c
Austin Paul McCartney <amccartney@lanl.gov>
2018-07-04 10:47:51 -0600

    fixed bug

--- Warnings/C/GNU.cmake
+++ Warnings/C/GNU.cmake
@@ -4,8 +4,8 @@ target_compile_options(warnings_C_GNU INTERFACE
   -Wall
   -Wextra
   -Wpedantic
-  $<$<VERSION_GREATER:$<CXX_COMPILER_VERSION>,7.0>:-Wduplicated-branches>
-  $<$<VERSION_GREATER:$<CXX_COMPILER_VERSION>,6.0>:-Wduplicated-cond>
+  $<$<VERSION_GREATER:$<C_COMPILER_VERSION>,7.0>:-Wduplicated-branches>
+  $<$<VERSION_GREATER:$<C_COMPILER_VERSION>,6.0>:-Wduplicated-cond>
   $<$<BOOL:$<TARGET_PROPERTY:werror>>:-Werror>)
 
 target_link_libraries(warnings_C INTERFACE
