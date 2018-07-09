fc686b768e181acc7aef7a23e6939486359f179b
Austin McCartney <amccartney@lanl.gov>
2018-07-09 09:07:37 -0600

    Update testname and resolve latent bugs

--- GeneratedSources/add_executable.cmake
+++ GeneratedSources/add_executable.cmake
@@ -15,4 +15,11 @@ function(add_executable target)
   previous_add_library(${stripped_target_name}.generated_sources.INTERFACE INTERFACE)
   target_link_libraries(${target} INTERFACE
     $<BUILD_INTERFACE:${stripped_target_name}.generated_sources.INTERFACE>)
+
+  export(TARGETS
+    ${stripped_target_name}.generated_sources.PUBLIC
+    ${stripped_target_name}.generated_sources.PRIVATE
+    ${stripped_target_name}.generated_sources.INTERFACE
+    NAMESPACE export::
+    FILE ${CMAKE_CURRENT_BINARY_DIR}/faux-exports/${stripped_target_name}.cmake)
 endfunction()
