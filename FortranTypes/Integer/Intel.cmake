c956a9e328da69de076c25bbb5a11ac679baf106
Austin Paul McCartney <amccartney@lanl.gov>
2018-07-19 17:38:39 -0600

    working around deduplication in cmake

--- FortranTypes/Integer/Intel.cmake
+++ FortranTypes/Integer/Intel.cmake
@@ -1,8 +1,7 @@
 string(CONCAT generator
   "$<$<STREQUAL:${CMAKE_Fortran_COMPILER_ID},Intel>"
     ":$<$<NOT:$<PLATFORM_ID:Windows>>"
-       ":$<$<BOOL:${FortranTypes.integer.32}>:-integer-size;32>;"
-        "$<$<BOOL:${FortranTypes.integer.64}>:-integer-size;64>>;"
+       ":$<$<BOOL:${FortranTypes.integer.64}>:-i8>>;"
      "$<$<PLATFORM_ID:Windows>"
        ":$<$<BOOL:${FortranTypes.integer.32}>:/integer-size:32>;"
         "$<$<BOOL:${FortranTypes.integer.64}>:/integer-size:64>>>")
--- FortranTypes/Real/Intel.cmake
+++ FortranTypes/Real/Intel.cmake
@@ -1,12 +1,8 @@
 string(CONCAT generator
   "$<$<AND:$<STREQUAL:${CMAKE_Fortran_COMPILER_ID},Intel>,"
           "$<NOT:$<PLATFORM_ID:Windows>>,"
-          "$<BOOL:${FortranTypes.real.32}>>"
-    ":-real-size;32>;"
-  "$<$<AND:$<STREQUAL:${CMAKE_Fortran_COMPILER_ID},Intel>,"
-          "$<NOT:$<PLATFORM_ID:Windows>>,"
           "$<BOOL:${FortranTypes.real.64}>>"
-    ":-real-size;64>;"
+    ":-r8>;"
   "$<$<AND:$<STREQUAL:${CMAKE_Fortran_COMPILER_ID},Intel>,"
           "$<PLATFORM_ID:Windows>,"
           "$<BOOL:${FortranTypes.real.32}>>"
