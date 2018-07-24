051f9e64c0a4fec9d41cde4110d3680d40bf2c6f
Clell J. (CJ) Solomon <csolomon@lanl.gov>
2018-07-24 11:57:48 -0600

    Implemented Intel FPModel for C & CXX

--- Intel/FPModel.cmake
+++ Intel/FPModel.cmake
@@ -1,7 +1,7 @@
 include(Backports/IncludeGuard)
 include_guard(GLOBAL)
 
-define_property(TARGET PROPERTY Intel_Fortran_FLOATING_POINT_MODEL
+define_property(TARGET PROPERTY Intel_FLOATING_POINT_MODEL
 BRIEF_DOCS
 "Intel Fortran floating point model"
 FULL_DOCS
@@ -14,14 +14,42 @@ set(intel.fpmodel.default "consistent" CACHE STRING
 )
 mark_as_advanced(intel.fpmodel.default)
 
+add_library(Intel_FPModel_C INTERFACE)
+add_library(Intel::FPModel_C ALIAS Intel_FPModel_C)
+
+string(CONCAT generator
+  "$<$<BOOL:$<TARGET_PROPERTY:Intel_FLOATING_POINT_MODEL>>:"
+    "$<$<STREQUAL:Intel,${CMAKE_C_COMPILER_ID}>:"
+      "$<$<NOT:$<PLATFORM_ID:Windows>>:-fp-model;$<TARGET_PROPERTY:Intel_FLOATING_POINT_MODEL>>"
+      "$<$<PLATFORM_ID:Windows>:/fp:$<TARGET_PROPERTY:Intel_FLOATING_POINT_MODEL>>"
+    ">"
+  ">"
+)
+
+target_compile_options(Intel_FPModel_C INTERFACE ${generator})
+
+add_library(Intel_FPModel_CXX INTERFACE)
+add_library(Intel::FPModel_CXX ALIAS Intel_FPModel_CXX)
+
+string(CONCAT generator
+  "$<$<BOOL:$<TARGET_PROPERTY:Intel_FLOATING_POINT_MODEL>>:"
+    "$<$<STREQUAL:Intel,${CMAKE_CXX_COMPILER_ID}>:"
+      "$<$<NOT:$<PLATFORM_ID:Windows>>:-fp-model;$<TARGET_PROPERTY:Intel_FLOATING_POINT_MODEL>>"
+      "$<$<PLATFORM_ID:Windows>:/fp:$<TARGET_PROPERTY:Intel_FLOATING_POINT_MODEL>>"
+    ">"
+  ">"
+)
+
+target_compile_options(Intel_FPModel_CXX INTERFACE ${generator})
+
 add_library(Intel_FPModel_Fortran INTERFACE)
 add_library(Intel::FPModel_Fortran ALIAS Intel_FPModel_Fortran)
 
 string(CONCAT generator
-  "$<$<BOOL:$<TARGET_PROPERTY:Intel_Fortran_FLOATING_POINT_MODEL>>:"
+  "$<$<BOOL:$<TARGET_PROPERTY:Intel_FLOATING_POINT_MODEL>>:"
     "$<$<STREQUAL:Intel,${CMAKE_Fortran_COMPILER_ID}>:"
-      "$<$<NOT:$<PLATFORM_ID:Windows>>:-fp-model;$<TARGET_PROPERTY:Intel_Fortran_FLOATING_POINT_MODEL>>"
-      "$<$<PLATFORM_ID:Windows>:/fp:$<TARGET_PROPERTY:Intel_Fortran_FLOATING_POINT_MODEL>>"
+      "$<$<NOT:$<PLATFORM_ID:Windows>>:-fp-model;$<TARGET_PROPERTY:Intel_FLOATING_POINT_MODEL>>"
+      "$<$<PLATFORM_ID:Windows>:/fp:$<TARGET_PROPERTY:Intel_FLOATING_POINT_MODEL>>"
     ">"
   ">"
 )
