ec524ce4c11e4f3a758c7a70c49b28acd16a541d
Austin Paul McCartney <amccartney@lanl.gov>
2018-07-24 21:08:02 -0600

    debuging on the HPC environment

--- Warnings/Fortran/Intel.cmake
+++ Warnings/Fortran/Intel.cmake
@@ -3,22 +3,22 @@ add_library(warnings_Fortran_Intel INTERFACE)
 string(CONCAT generator
   "$<$<BOOL:$<TARGET_PROPERTY:WARN_ERROR>>"
    ":$<IF:$<PLATFORM_ID:Windows>"
-        ",$<1:/warn:error;/warn:stderror>"
+        ",$<1:/warn$<1::>error;/warn$<1::>stderror>"
         ",$<1:-warn;error;-warn;stderror>"
      ">"
    ">"
   "$<$<BOOL:$<TARGET_PROPERTY:WARN_ALL>>"
    ":$<IF:$<PLATFORM_ID:Windows>"
-        ",$<1:/warn:all>"
+        ",$<1:/warn$<1::>all>"
         ",$<1:-warn;all>"
      ">"
    ">;"
   "$<$<BOOL:$<TARGET_PROPERTY:INTEL_ENABLED_WARNINGS>>:"
-    "$<IF:$<PLATFORM_ID:Windows>,/warn:,-warn;>"
-    "$<JOIN:$<TARGET_PROPERTY:INTEL_ENABLED_WARNINGS>,$<comma>>>;"
+    "$<IF:$<PLATFORM_ID:Windows>,/warn$<1::>,-warn;>"
+    "$<JOIN:$<TARGET_PROPERTY:INTEL_ENABLED_WARNINGS>,$<COMMA>>>;"
   "$<$<BOOL:$<TARGET_PROPERTY:INTEL_DISABLED_WARNINGS>>:"
-    "$<IF:$<PLATFORM_ID:Windows>,/nowarn:,-nowarn;>"
-    "$<JOIN:$<TARGET_PROPERTY:INTEL_DISABLED_WARNINGS>,$<comma>>>;")
+    "$<IF:$<PLATFORM_ID:Windows>,/nowarn$<1::>,-nowarn;>"
+    "$<JOIN:$<TARGET_PROPERTY:INTEL_DISABLED_WARNINGS>,$<COMMA>>>;")
 
 target_compile_options(warnings_Fortran INTERFACE
   $<$<STREQUAL:${CMAKE_Fortran_COMPILER_ID},Intel>:${generator}>)
