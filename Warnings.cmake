ba464ca1fa8a92dc253e7a131fb714dc8bc524b5
Austin McCartney <amccartney@lanl.gov>
2018-07-24 20:28:07 -0600

    Refactor of the shacl::cmake::Warnings module
    Collapses `CompileOptions/Intel/DiagDisable.cmake` and
    `CompileOptions/Intel/DiagDisable.cmake` to the Warnings module
    and adds analogous functionality for MSVC, GCC, and LLVM. Adds
    the following properties:
    WARN_ALL
    WARN_ERROR
    GNU_ENABLED_WARNINGS
    GNU_DISABLED_WARNINGS
    LLVM_ENABLED_WARNINGS
    LLVM_DISABLED_WARNINGS
    MSVC_ENABLED_WARNINGS
    MSVC_DISABLED_WARNINGS
    INTEL_ENABLED_WARNINGS
    INTEL_DISABLED_WARNINGS

--- Warnings.cmake
+++ Warnings.cmake
@@ -1,6 +1,106 @@
 include(Backports/IncludeGuard)
 include_guard(GLOBAL)
 
+define_property(TARGET PROPERTY WARN_ALL
+BRIEF_DOCS
+"Use all available warning flags subject to few or no false positives"
+FULL_DOCS
+"This boolean flag is used to provide a reasonably complete set of flags
+ for each compiler vendor for each language")
+
+define_property(TARGET PROPERTY WARN_ERROR
+BRIEF_DOCS
+"Treat warnings as errors"
+FULL_DOCS
+"When the boolean property is set, when supported by the compiler in use,
+ compiler warnings will result in a compilation errors")
+
+define_property(TARGET PROPERTY GNU_ENABLED_WARNINGS
+BRIEF_DOCS
+"GNU warning keywords flags"
+FULL_DOCS
+"This property contains zero or more entries specifying warning options
+ used when compiling with an GNU compiler. Each entry corresponds to a
+ warning keyword, e.g., specifying `strict-aliasing` will add the
+ `-Wstrict-aliasing` flag.
+
+ Multiple entries must be semicolon separated e.g. unused;unused")
+
+define_property(TARGET PROPERTY GNU_DISABLED_WARNINGS
+BRIEF_DOCS
+"GNU warning keywords flags"
+FULL_DOCS
+"This property contains zero or more entries specifying warning options
+ used when compiling with an GNU compiler. Each entry corresponds to a
+ warning keyword, e.g., specifying `strict-aliasing` will add the
+ `-Wno-strict-aliasing` flag.
+
+ Multiple entries must be semicolon separated e.g. unused;unused")
+
+define_property(TARGET PROPERTY LLVM_ENABLED_WARNINGS
+BRIEF_DOCS
+"LLVM warning keywords flags"
+FULL_DOCS
+"This property contains zero or more entries specifying warning options
+ used when compiling with an LLVM compiler. Each entry corresponds to a
+ warning keyword, e.g., specifying `strict-aliasing` will add the
+ `-Wno-strict-aliasing` flag.
+
+ Multiple entries must be semicolon separated e.g. unused;unused")
+
+define_property(TARGET PROPERTY LLVM_DISABLED_WARNINGS
+BRIEF_DOCS
+"LLVM warning keywords flags"
+FULL_DOCS
+"This property contains zero or more entries specifying warning options
+ used when compiling with an LLVM compiler. Each entry corresponds to a
+ warning keyword, e.g., specifying `strict-aliasing` will add the
+ `-Wno-strict-aliasing` flag.
+
+ Multiple entries must be semicolon separated e.g. unused;unused")
+
+define_property(TARGET PROPERTY MSVC_ENABLED_WARNINGS
+BRIEF_DOCS
+"MSVC warning keywords flags"
+FULL_DOCS
+"This property contains zero or more entries specifying warning options
+ used when compiling with an MSVC compiler. Each entry corresponds to a
+ warning keyword, e.g., specifying `4326` will add the `/we4326` flag.
+
+ Multiple entries must be semicolon separated e.g. unused;unused")
+
+define_property(TARGET PROPERTY MSVC_DISABLED_WARNINGS
+BRIEF_DOCS
+"MSVC warning keywords flags"
+FULL_DOCS
+"This property contains zero or more entries specifying warning options
+ used when compiling with an MSVC compiler. Each entry corresponds to a
+ warning keyword, e.g., specifying `4326` will add the `/wd4326` flag.
+
+ Multiple entries must be semicolon separated e.g. unused;unused")
+
+define_property(TARGET PROPERTY Intel_ENABLED_WARNINGS
+BRIEF_DOCS
+"Intel warning keywords flags"
+FULL_DOCS
+"This property contains zero or more entries specifying warning options
+ used when compiling with an Intel compiler. Each entry corresponds to a
+ warning keyword, e.g., specifying `uncalled` will add the `/warn:uncalled`
+ flag or `-warn uncalled` flag as appropriate for the host platform.
+
+ Multiple entries must be semicolon separated e.g. unused;unused")
+
+define_property(TARGET PROPERTY Intel_DISABLED_WARNINGS
+BRIEF_DOCS
+"Intel warning keywords flags"
+FULL_DOCS
+"This property contains zero or more entries specifying warning options
+ used when compiling with an Intel compiler. Each entry corresponds to a
+ warning keyword, e.g., specifying `uncalled` will add the `/warn:no-uncalled`
+ flag or `-warn no-uncalled` flag as appropriate for the host platform.
+
+ Multiple entries must be semicolon separated e.g. unused;unused")
+
 add_library(warnings_C INTERFACE)
 include(Warnings/C)
 
@@ -10,6 +110,6 @@ include(Warnings/CXX)
 add_library(warnings_Fortran INTERFACE)
 include(Warnings/Fortran)
 
-add_library(shacl::warnings_C ALIAS warnings_C)
-add_library(shacl::warnings_CXX ALIAS warnings_CXX)
-add_library(shacl::warnings_Fortran ALIAS warnings_Fortran)
+add_library(warnings::warnings_C ALIAS warnings_C)
+add_library(warnings::warnings_CXX ALIAS warnings_CXX)
+add_library(warnings::warnings_Fortran ALIAS warnings_Fortran)
--- Warnings/Fortran/Intel.cmake
+++ Warnings/Fortran/Intel.cmake
@@ -1,16 +1,24 @@
 add_library(warnings_Fortran_Intel INTERFACE)
 
 string(CONCAT generator
-  "$<IF:$<PLATFORM_ID:Windows>"
-      ",$<1:/W1;/warn:all>"
-      ",$<1:-W1;-warn;all>"
+  "$<$<BOOL:$<TARGET_PROPERTY:WARN_ERROR>>"
+   ":$<IF:$<PLATFORM_ID:Windows>"
+        ",$<1:/warn:error;/warn:stderror>"
+        ",$<1:-warn;error;-warn;stderror>"
+     ">"
+   ">"
+  "$<$<BOOL:$<TARGET_PROPERTY:WARN_ALL>>"
+   ":$<IF:$<PLATFORM_ID:Windows>"
+        ",$<1:/warn:all>"
+        ",$<1:-warn;all>"
+     ">"
    ">;"
-  "$<$<BOOL:$<TARGET_PROPERTY:werror>>"
-   ":$<IF:$<PLATFORM_ID:Windows>,"
-         "$<1:/warn:error;/warn:stderror>,"
-         "$<1:-warn error;-warn stderror>>>")
+  "$<$<BOOL:$<TARGET_PROPERTY:INTEL_ENABLED_WARNINGS>>:"
+    "$<IF:$<PLATFORM_ID:Windows>,/warn:,-warn;>"
+    "$<JOIN:$<TARGET_PROPERTY:INTEL_ENABLED_WARNINGS>,$<comma>>>;"
+  "$<$<BOOL:$<TARGET_PROPERTY:INTEL_DISABLED_WARNINGS>>:"
+    "$<IF:$<PLATFORM_ID:Windows>,/nowarn:,-nowarn;>"
+    "$<JOIN:$<TARGET_PROPERTY:INTEL_DISABLED_WARNINGS>,$<comma>>>;")
 
-target_compile_options(warnings_Fortran_Intel INTERFACE ${generator})
-
-target_link_libraries(warnings_Fortran INTERFACE
-  $<$<STREQUAL:${CMAKE_Fortran_COMPILER_ID},Intel>:warnings_Fortran_Intel>)
+target_compile_options(warnings_Fortran INTERFACE
+  $<$<STREQUAL:${CMAKE_Fortran_COMPILER_ID},Intel>:${generator}>)
