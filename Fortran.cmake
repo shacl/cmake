7d6848c3c45c9ae5c2ceb058644db2d2ca28294e
Clell J. (CJ) Solomon <csolomon@lanl.gov>
2018-07-25 07:53:43 -0600

    Provide Fortran::StandardAdherence meta-target

--- Fortran.cmake
+++ Fortran.cmake
@@ -1,6 +1,10 @@
+include(Backports/IncludeGuard)
+include_guard(GLOBAL)
+
 include(Fortran/Backtrace)
 include(Fortran/Standard)
 include(Fortran/TempArrayHeapAlloc)
 include(Fortran/NoBackslashEscape)
 include(Fortran/Integer)
 include(Fortran/Real)
+include(Fortran/StandardAdherence)
