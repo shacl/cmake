82461d31e436b9f224b3adbf2f9c236315026fc9
Austin McCartney <amccartney@lanl.gov>
2018-07-25 21:17:15 -0600

    intel fortran work

--- Warnings/Fortran/Intel.cmake
+++ Warnings/Fortran/Intel.cmake
@@ -1,23 +1,27 @@
 add_library(warnings_Fortran_Intel INTERFACE)
 
 string(CONCAT generator
+  "$<IF:$<PLATFORM_ID:Windows>"
+      ",/warn$<1::>"
+      ",-warn;"
+   ">")
+
+string(CONCAT generator
   "$<$<BOOL:$<TARGET_PROPERTY:WARN_ERROR>>"
-   ":$<IF:$<PLATFORM_ID:Windows>"
-        ",$<1:/warn$<1::>error;/warn$<1::>stderror>"
-        ",$<1:-warn;error;-warn;stderror>"
-     ">"
+   ":${generator}error$<COMMA>stderror"
    ">"
   "$<$<BOOL:$<TARGET_PROPERTY:WARN_ALL>>"
-   ":$<IF:$<PLATFORM_ID:Windows>"
-        ",$<1:/warn$<1::>all>"
-        ",$<1:-warn;all>"
+   ":$<IF:$<BOOL:$<TARGET_PROPERTY:WARN_ERROR>>"
+        ",$<COMMA>"
+        ",${generator}"
      ">"
+     "all"
    ">;"
   "$<$<BOOL:$<TARGET_PROPERTY:INTEL_ENABLED_WARNINGS>>:"
-    "$<IF:$<PLATFORM_ID:Windows>,/warn$<1::>,-warn;>"
+    "$<IF:$<PLATFORM_ID:Windows>,/Qdiag-enable$<1::>,-diag-enable=>"
     "$<JOIN:$<TARGET_PROPERTY:INTEL_ENABLED_WARNINGS>,$<COMMA>>>;"
   "$<$<BOOL:$<TARGET_PROPERTY:INTEL_DISABLED_WARNINGS>>:"
-    "$<IF:$<PLATFORM_ID:Windows>,/nowarn$<1::>,-nowarn;>"
+    "$<IF:$<PLATFORM_ID:Windows>,/Qdiag-disable$<1::>,-diag-disable=>"
     "$<JOIN:$<TARGET_PROPERTY:INTEL_DISABLED_WARNINGS>,$<COMMA>>>;")
 
 target_compile_options(warnings_Fortran INTERFACE
