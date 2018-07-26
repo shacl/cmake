d14c7592a1d5a8e3ff1c70bf763159a809327e0e
Austin McCartney <amccartney@lanl.gov>
2018-07-26 12:42:54 -0600

    updated intel standard adherence cmake module test

--- Intel/FortranStandardAdherence.cmake
+++ Intel/FortranStandardAdherence.cmake
@@ -4,19 +4,19 @@ include_guard(GLOBAL)
 
 # Standard-semantics options for Intel
 
+add_library(Intel_Fortran_StandardAdherence INTERFACE)
+add_library(Intel::FortranStandardAdherence ALIAS Intel_Fortran_StandardAdherence)
+
+set_target_properties(Intel_Fortran_StandardAdherence PROPERTIES
+  INTERFACE_Intel_FORTRAN_DISABLED_ASSUMPTIONS std_mod_proc_name)
+set_property(TARGET Intel_Fortran_StandardAdherence APPEND PROPERTY
+  COMPATIBLE_INTERFACE_STRING Intel_FORTRAN_DISABLED_ASSUMPTIONS)
+
 string(CONCAT generator
   "$<$<STREQUAL:Intel,${CMAKE_Fortran_COMPILER_ID}>:"
       "$<$<NOT:$<PLATFORM_ID:Windows>>:-standard-semantics;>"
           "$<$<PLATFORM_ID:Windows>:/standard-semantics;>>")
 
-add_library(Intel_Fortran_StandardAdherence INTERFACE)
-add_library(Intel::FortranStandardAdherence ALIAS Intel_Fortran_StandardAdherence)
-
-set_target_properties(Intel_Fortran_StandardAdherence PROPERTIES
-  INTERFACE_Intel_DISABLED_ASSUMPTIONS std_mod_proc_name)
-set_property(TARGET Intel_Fortran_StandardAdherence APPEND PROPERTY
-  COMPATIBLE_INTERFACE_STRING Intel_DISABLED_ASSUMPTIONS)
-
-target_compile_options(Intel_Fortran_StandardAdherence INTERFACE
-  ${generator})
-target_link_libraries(Intel_Fortran_StandardAdherence INTERFACE Intel::FortranAssumptions)
+target_compile_options(Intel_Fortran_StandardAdherence INTERFACE ${generator})
+target_link_libraries(Intel_Fortran_StandardAdherence
+  INTERFACE Intel::FortranAssumptions)
