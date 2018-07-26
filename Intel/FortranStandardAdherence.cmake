b3600fffba3d4e61fcc0e59b375f7e017ab1f801
Austin Paul McCartney <amccartney@lanl.gov>
2018-07-25 23:48:46 -0600

    Ported FortranStandardAdherence target to use Intel::Assumptions target to avoid deduplication

--- Intel/FortranStandardAdherence.cmake
+++ Intel/FortranStandardAdherence.cmake
@@ -1,15 +1,22 @@
 include(Backports/IncludeGuard)
+include(Intel/Assumptions)
 include_guard(GLOBAL)
 
 # Standard-semantics options for Intel
 
 string(CONCAT generator
   "$<$<STREQUAL:Intel,${CMAKE_Fortran_COMPILER_ID}>:"
-      "$<$<NOT:$<PLATFORM_ID:Windows>>:-standard-semantics;-assume;nostd_mod_proc_name>"
-          "$<$<PLATFORM_ID:Windows>:/standard-semantics;/assume:nostd_mod_proc_name>>")
+      "$<$<NOT:$<PLATFORM_ID:Windows>>:-standard-semantics;>"
+          "$<$<PLATFORM_ID:Windows>:/standard-semantics;>>")
 
 add_library(Intel_Fortran_StandardAdherence INTERFACE)
 add_library(Intel::FortranStandardAdherence ALIAS Intel_Fortran_StandardAdherence)
+
+set_target_properties(Intel_Fortran_StandardAdherence PROPERTIES 
+  INTERFACE_Intel_DISABLED_ASSUMPTIONS std_mod_proc_name)
+set_property(TARGET Intel_Fortran_StandardAdherence APPEND PROPERTY
+  COMPATIBLE_INTERFACE_STRING Intel_DISABLED_ASSUMPTIONS)
+
 target_compile_options(Intel_Fortran_StandardAdherence INTERFACE
-  ${generator}
-)
+  ${generator})
+target_link_libraries(Intel_Fortran_StandardAdherence INTERFACE Intel::Assumptions)
