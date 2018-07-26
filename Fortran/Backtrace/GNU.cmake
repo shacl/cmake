897c5cdede954d0675f22b94586f111580b7a316
Austin Paul McCartney <amccartney@lanl.gov>
2018-07-26 07:50:21 -0600

    Refactored the Fortran Backtrace CMake module

--- Fortran/Backtrace/GNU.cmake
+++ Fortran/Backtrace/GNU.cmake
@@ -1,8 +1,8 @@
 string(CONCAT generator
-  "$<$<STREQUAL:PGI,${CMAKE_Fortran_COMPILER_ID}>"
+  "$<$<STREQUAL:GNU,${CMAKE_Fortran_COMPILER_ID}>"
    ":$<IF:$<BOOL:$<TARGET_PROPERTY:Fortran_BACKTRACE>>"
-        ",-traceback;"
-        ",-notraceback;"
+        ",-fbacktrace;"
+        ",-fno-backtrace;"
      ">"
    ">")
 
