96325898d30348307880c14d8a3fce568d0aa518
Austin Paul McCartney <amccartney@lanl.gov>
2018-10-25 13:49:47 -0600

    Tests passing local

--- Git/Submodule/Packages/collect_state.cmake
+++ Git/Submodule/Packages/collect_state.cmake
@@ -1,6 +1,5 @@
 # This macro populates the current_hash and current_branch variables
 macro(git_submodule_collect_state name)
-  message(STATUS "git_submodule_collect_state: ${git_submodule_collect_state}")
   execute_process(
     COMMAND ${GIT_EXECUTABLE} rev-parse HEAD
     WORKING_DIRECTORY "${${name}.submodule.path}"
@@ -16,7 +15,7 @@ macro(git_submodule_collect_state name)
     message(FATAL_ERROR ${error_output})
   endif()
 
-  set(${name}.submodule.hash ${current_hash} CACHE STRING
+  set(${name}.submodule.hash "${current_hash}" CACHE STRING
     "${name} git submodule hash")
 
   mark_as_advanced(${name}.submodule.hash)
