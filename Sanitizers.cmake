c3428d2311cc59fc924b8bc1ac663721c568a52b
Clell J. (CJ) Solomon <csolomon@lanl.gov>
2019-02-15 21:06:00 -0700

    Sanitizers install logic

--- Sanitizers.cmake
+++ Sanitizers.cmake
@@ -1,4 +1,3 @@
-include(Backports/IncludeGuard)
 include_guard(GLOBAL)
 
 define_property(TARGET PROPERTY asan
@@ -30,8 +29,15 @@ option(ubsan.default
   "Default undefined behavior sanitizer behavior (ON/OFF)" OFF)
 mark_as_advanced(ubsan.default)
 
-add_library(sanitizers INTERFACE)
-add_library(shacl::sanitizers ALIAS sanitizers)
+add_library(shacl::cmake::sanitizers INTERFACE IMPORTED GLOBAL)
+
+# These aliases are provided for short term backwards compatability.
+#
+# Please don't not use in new work and update existing work to use the
+# the imported target defined above as soon as reasonably possible.
+
+add_library(sanitizers ALIAS shacl::cmake::sanitizers)
+add_library(shacl::sanitizers ALIAS shacl::cmake::sanitizers)
 
 set(asan address)
 set(msan memory)
@@ -54,15 +60,21 @@ foreach(sanitizer IN ITEMS asan msan tsan ubsan)
       ">:-fsanitize=${${sanitizer}}>;")
 endforeach()
 
-target_compile_options(sanitizers INTERFACE ${compilation_generator})
-target_link_libraries(sanitizers INTERFACE ${linking_generator})
+target_compile_options(shacl::cmake::sanitizers INTERFACE ${compilation_generator})
+target_link_libraries(shacl::cmake::sanitizers INTERFACE ${linking_generator})
 
-add_library(sanitizers_C INTERFACE)
-add_library(sanitizers_CXX INTERFACE)
-add_library(sanitizers_Fortran INTERFACE)
-add_library(shacl::sanitizers_C ALIAS sanitizers_C)
-add_library(shacl::sanitizers_CXX ALIAS sanitizers_CXX)
-add_library(shacl::sanitizers_Fortran ALIAS sanitizers_Fortran)
+add_library(shacl::cmake::sanitizers_C INTERFACE IMPORTED GLOBAL)
+add_library(shacl::cmake::sanitizers_CXX INTERFACE IMPORTED GLOBAL)
+add_library(shacl::cmake::sanitizers_Fortran INTERFACE IMPORTED GLOBAL)
+
+# These aliases are provided for short term backwards compatability.
+#
+# Please don't not use in new work and update existing work to use the
+# the imported target defined above as soon as reasonably possible.
+
+add_library(shacl::sanitizers_C ALIAS shacl::cmake::sanitizers_C)
+add_library(shacl::sanitizers_CXX ALIAS shacl::cmake::sanitizers_CXX)
+add_library(shacl::sanitizers_Fortran ALIAS shacl::cmake::sanitizers_Fortran)
 
 get_property(languages GLOBAL PROPERTY ENABLED_LANGUAGES)
 foreach(language IN ITEMS C CXX Fortran)
@@ -73,6 +85,10 @@ foreach(language IN ITEMS C CXX Fortran)
       "$<STREQUAL:Clang,${CMAKE_${language}_COMPILER_ID}>,"
       "$<STREQUAL:AppleClang,${CMAKE_${language}_COMPILER_ID}>>"
       ":shacl::sanitizers>")
-    target_link_libraries(sanitizers_${language} INTERFACE ${vendor_discrimination})
+    target_link_libraries(shacl::cmake::sanitizers_${language} INTERFACE ${vendor_discrimination})
   endif()
 endforeach()
+
+install(FILES
+  ${CMAKE_CURRENT_LIST_DIR}/Sanitizers.cmake
+  DESTINATION share/cmake/shacl/.cmake)
