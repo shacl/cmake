b6d4330770f930c0ae4b1ad1bfb0dbb38db7fe98
Clell J. (CJ) Solomon <csolomon@lanl.gov>
2019-02-20 13:27:37 -0700

    Remove unneeded Backports with CMake 3.12 requirement

--- Backports.cmake
+++ Backports.cmake
@@ -1,10 +1,6 @@
 set_property( GLOBAL PROPERTY FIND_LIBRARY_USE_LIB64_PATHS ON )
 
-include(Backports/OpenMP)
-include(Backports/MPI)
 include(Backports/X11)
-include(Backports/SeparateArguments)
-include(Backports/IncludeGuard)
 
 install(FILES
   ${CMAKE_CURRENT_LIST_DIR}/Backports.cmake
