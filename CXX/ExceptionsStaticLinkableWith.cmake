37d7ea5789aad2bfeb43f82262f84d960928fd19
Clell J. (CJ) Solomon <csolomon@lanl.gov>
2019-03-05 13:42:31 -0700

    Updated function names and removed debug prints

--- CXX/ExceptionsStaticLinkableWith.cmake
+++ CXX/ExceptionsStaticLinkableWith.cmake
@@ -2,11 +2,11 @@ cmake_minimum_required(VERSION 3.12.1)
 include_guard(GLOBAL)
 
 set(CXX_exceptions_static_linkable_root_dir 
-  ${CMAKE_CURRENT_LIST_DIR}/ExceptionsStaticLinkable
+  ${CMAKE_CURRENT_LIST_DIR}/ExceptionsStaticLinkableWith
   CACHE INTERNAL ""
 )
 
-function(CXX_exceptions_static_linkable)
+function(CXX_exceptions_static_linkable_with)
   foreach(lang IN LISTS ARGN)
     if( DEFINED CXX_exceptions_static_linkable_with_${lang} )
       continue()
@@ -16,10 +16,9 @@ function(CXX_exceptions_static_linkable)
       ${CXX_exceptions_static_linkable_root_dir}/CXX_exceptions_${lang}_static_linkable
     )
 
-    message("test_project_source_dir = ${test_project_source_dir}")
     if( NOT EXISTS ${test_project_source_dir} )
       message(FATAL_ERROR
-        "A test project for linking CXX static library code with exceptions using ${lang} does not exists")
+        "A test project for linking CXX static library code with exceptions using ${lang} does not exists at ${test_project_source_dir}")
     endif()
 
     try_compile(compatible
