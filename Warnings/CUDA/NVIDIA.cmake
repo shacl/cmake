341870d53419bb1066e57cd17432205933390f60
Tim Burke <tpburke@lanl.gov>
2019-03-25 12:56:05 -0600

    Removes enable_language from NVIDIA.cmake

--- Warnings/CUDA/NVIDIA.cmake
+++ Warnings/CUDA/NVIDIA.cmake
@@ -4,6 +4,5 @@ string(CONCAT generator
   "$<JOIN:$<GENEX_EVAL:$<TARGET_PROPERTY:Warnings_CXX,INTERFACE_COMPILE_OPTIONS>>,$<COMMA>>"
   "$<$<CXX_COMPILER_ID:GNU>:,-Wno-pedantic>")
 
-enable_language(CUDA)
 target_compile_options(shacl::cmake::Warnings_CUDA INTERFACE
   $<$<STREQUAL:${CMAKE_CUDA_COMPILER_ID},NVIDIA>:${generator}>)
