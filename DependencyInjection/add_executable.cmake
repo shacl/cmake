c91235b67436a27d06ddf263ec658a5bf2402778
Simon R. Bolding <sbolding@lanl.gov>
2019-06-03 11:30:15 -0600

    Fixed error in tests that depended on internal variables

--- DependencyInjection/add_executable.cmake
+++ DependencyInjection/add_executable.cmake
@@ -5,7 +5,7 @@ macro(add_executable target_)
   push(scl.DependencyInjection.target)
   set(scl.DependencyInjection.target ${target_})
 
-  previous_add_executable(${scl.DependencyInjection.target} "${ARGN}")
+  previous_add_executable(${target_} "${ARGN}")
 
   foreach(inc IN LISTS ${scl.DependencyInjection.target}.injections)
     include(${inc})
