2bf471c3331fdac67c5015d001a3f655a9b84205
Austin McCartney <amccartney@lanl.gov>
2019-06-05 17:32:21 -0600

    Added DelegatingSelection

---
+++ DelegatingSelection.cmake
@@ -0,0 +1,54 @@
+cmake_minimum_required(VERSION 3.12.1)
+include_guard(GLOBAL)
+
+function(delegating_selection variable)
+  set(OPTIONS)
+  set(UNARY_ARGUMENTS DEFAULT DOCSTRING)
+  set(VARIADIC_ARGUMENTS OPTIONS)
+
+  cmake_parse_arguments(ds
+    "${OPTIONS}"
+    "${UNARY_ARGUMENTS}"
+    "${VARIADIC_ARGUMENTS}" ${ARGN})
+
+  if(NOT DEFINED ds_DEFAULT)
+    message(FATAL_ERROR
+      "delegating_selection invoked without 'DEFAULT' argument")
+  endif()
+
+  if(ds_DOCSTRING)
+    string(CONCAT docstring
+      "${ds_DOCSTRING}\n"
+      "When set to 'default', ${variable} assumes the value of ${ds_DEFAULT}")
+  else()
+    string(CONCAT docstring
+      "When set to 'default', ${variable} assumes the value of ${ds_DEFAULT}")
+  endif()
+
+  if(NOT DEFINED ds_OPTIONS)
+    message(FATAL_ERROR "selection invoked without 'OPTIONS' keyword")
+  endif()
+
+  set(${variable} "default" CACHE STRING "${docstring}")
+  set(options default;${ds_OPTIONS})
+  set_property(CACHE ${variable} PROPERTY STRINGS ${options})
+
+  if(${variable} STREQUAL "default")
+    set(${variable} "${${ds_DEFAULT}}")
+  endif()
+
+  if(NOT ${variable} IN_LIST options)
+    message("${variable} set to ${${variable}}")
+    set(error_message "${variable} must be one of:")
+    foreach(option_string IN LISTS options)
+      string(CONCAT error_message "${error_message}\n" "  ${option_string}")
+    endforeach()
+    message(FATAL_ERROR "${error_message}")
+  endif()
+
+  set(${variable} "${${variable}}" PARENT_SCOPE)
+endfunction()
+
+install(FILES
+  "${CMAKE_CURRENT_LIST_DIR}/DelegatingSelection.cmake"
+  DESTINATION share/cmake/shacl/.cmake)
