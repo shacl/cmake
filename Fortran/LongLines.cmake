fcb1400890b00f7d33b2e7e502be21baf47d8fd4
Anthony J Zukaitis <zukaitis@lanl.gov>
2019-06-10 14:21:20 -0600

    Removed backwards compatible interface...

--- Fortran/LongLines.cmake
+++ Fortran/LongLines.cmake
@@ -10,14 +10,6 @@ add_library(shacl::cmake::Fortran::LongLines INTERFACE IMPORTED GLOBAL)
 
 include(Fortran/LongLines/GNU)
 
-# These aliases are provided for short term backwards compatability.
-#
-# Please don't not use in new work and update existing work to use the
-# the imported target defined above as soon as reasonably possible.
-
-add_library(Fortran_LongLines ALIAS shacl::cmake::Fortran::LongLines)
-add_library(Fortran::LongLines ALIAS shacl::cmake::Fortran::LongLines)
-
 install(FILES
   "${CMAKE_CURRENT_LIST_DIR}/LongLines.cmake"
   DESTINATION share/cmake/shacl/.cmake/Fortran)
